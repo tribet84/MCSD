f84a8d088b6dbd3a23890ae3079b243af55a74d8
roberto.rabasco <roberto.rabasco@belron.com>
2015-08-02 14:49:50 +0100

    Added "types" examples first part

---
+++ Chapter2/Part 1/Types1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class Types1
+    {
+        enum Days : byte
+        {
+            Sat = 1,
+            Sun,
+            Mon,
+            Tue,
+            Wed,
+            Thu,
+            Fri
+        };
+
+        public static void Run()
+        {
+            // to compare we need to cast to type: "byte"
+            Days day = Days.Mon;
+            if ((byte)day == 3)
+            {
+                Console.WriteLine("Today is Monday");
+            }
+        }
+    }
+}
+// output
+// Today is Monday 
--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -10,6 +10,8 @@ namespace Chapter2
     {
         static void Main(string[] args)
         {
+            Types2.Run();
+            Console.Read();
         }
     }
 }

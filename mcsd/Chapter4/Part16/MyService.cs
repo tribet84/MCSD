8994a15f299f847a46f6d1db32da2b460a97521c
robertorabasco <robesms@gmail.com>
2015-10-28 11:29:56 +0000

    Add chapter 16 examples for data

---
+++ mcsd/Chapter4/Part16/MyService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+
+namespace Chapter4
+{
+    [ServiceContract]
+    class MyService
+    {
+        [OperationContract]
+        public string DoSomeWork(string left, string right)
+        {
+            // Add System.ServiceModel reference to your project
+            return string.Format("{0}, {1}", left, right);
+        }
+    }
+}
